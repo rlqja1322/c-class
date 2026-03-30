b65ed160866ac972090704820d192a88d0580f18
ygibeom <rlqja1322@gmail.com>
2026-03-30 14:47:03 +0900

    문자 배열 실습

---
+++ array/array/char-array-1.c
@@ -0,0 +1,18 @@
+﻿//* 문자열 ppt -> 5번 7번 9번 슬라이드
+// 문자열의 길이를 구하는 프로그램
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    char str[30] = "C language";//문자배열 30자
+    int i = 0;
+
+    while (str[i] != '\0')//만족할때 반복
+                          // != 다르다 \0는 null값
+                          //문자열 내용 끝나는 시점 반드시 null값이 들어감
+        i++;
+
+    printf("문자열\"%s\"의 길이는 %d입니다.\n", str, i);
+    return 0;
+}
---
+++ array/array/char-array-2.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h>
+int main(void) {
+
+	int i;
+	char fruits[3][20];
+	for (i = 0; i < 3; i++){
+		printf("첫번째 과일 이름을 입력하세요 : ");
+	scanf("%s", fruits[i]);
+	printf("%d 번째 과일은 : %s \n", i, fruits[1]);
+}
+
+
+	return 0;
+}
